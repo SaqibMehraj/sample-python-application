eda440aad078a4b995c219e2932669fb644dce9c
SaqibMehraj <84028792+SaqibMehraj@users.noreply.github.com>
2023-11-10 11:31:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        message = 'Hello, World!'
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    print('Server running at localhost:8000...')
+    httpd.serve_forever()
+
+run()
